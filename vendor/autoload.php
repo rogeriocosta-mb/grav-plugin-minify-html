da641d39045ffa27c9cf651176c43b8122ed0491
jimblue <mr.jimblue@gmail.com>
2019-01-29 12:37:40 +0100

    prepare for release

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit3dd0f8d48a81da0a3c9fdc59d11c5654::getLoader();
+return ComposerAutoloaderInit49f522add605a9839e317a7f7fe1c82d::getLoader();
